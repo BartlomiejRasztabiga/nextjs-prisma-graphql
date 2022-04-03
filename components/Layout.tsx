13e56e7f246fb201c6b2a603cf1677a35e3c932b
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-03 20:22:39 +0200

    WIP

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,84 +1,199 @@
-import Link from "next/link";
-import {signOut} from "next-auth/react";
-import {useRouter} from "next/router";
-import Navigation from "./Navigation";
+/* This example requires Tailwind CSS v2.0+ */
+import {Fragment} from 'react'
+import {Disclosure, Menu, Transition} from '@headlessui/react'
+import {BellIcon, MenuIcon, XIcon} from '@heroicons/react/outline'
 
-const Layout = (props) => {
-        const router = useRouter();
+const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+    {name: 'Dashboard', href: '#', current: true},
+    {name: 'Team', href: '#', current: false},
+    {name: 'Projects', href: '#', current: false},
+    {name: 'Calendar', href: '#', current: false},
+    {name: 'Reports', href: '#', current: false},
+]
+const userNavigation = [
+    {name: 'Your Profile', href: '#'},
+    {name: 'Settings', href: '#'},
+    {name: 'Sign out', href: '#'},
+]
 
-        function isActive(pathname) {
-            return router.pathname === pathname;
-        }
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
-        return (
-            <div>
-                <div className="drawer">
-                    <input id="my-drawer-3" type="checkbox" className="drawer-toggle"/>
-                    <div className="drawer-content flex flex-col">
-                        <div className="w-full navbar bg-base-300">
-                            <div className="flex-none lg:hidden">
-                                <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         className="inline-block w-6 h-6 stroke-current">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M4 6h16M4 12h16M4 18h16"/>
-                                    </svg>
-                                </label>
+export default function Layout(props) {
+    return (
+        <>
+            <div className="min-h-full">
+                <Disclosure as="nav" className="bg-gray-800">
+                    {({open}) => (
+                        <>
+                            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                                <div className="flex items-center justify-between h-16">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0">
+                                            <img
+                                                className="h-8 w-8"
+                                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                                alt="Workflow"
+                                            />
+                                        </div>
+                                        <div className="hidden md:block">
+                                            <div className="ml-10 flex items-baseline space-x-4">
+                                                {navigation.map((item) => (
+                                                    <a
+                                                        key={item.name}
+                                                        href={item.href}
+                                                        className={classNames(
+                                                            item.current
+                                                                ? 'bg-gray-900 text-white'
+                                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'px-3 py-2 rounded-md text-sm font-medium'
+                                                        )}
+                                                        aria-current={item.current ? 'page' : undefined}
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="hidden md:block">
+                                        <div className="ml-4 flex items-center md:ml-6">
+                                            <button
+                                                type="button"
+                                                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                            >
+                                                <span className="sr-only">View notifications</span>
+                                                <BellIcon className="h-6 w-6" aria-hidden="true"/>
+                                            </button>
+
+                                            {/* Profile dropdown */}
+                                            <Menu as="div" className="ml-3 relative">
+                                                <div>
+                                                    <Menu.Button
+                                                        className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                                        <span className="sr-only">Open user menu</span>
+                                                        <img className="h-8 w-8 rounded-full" src={user.imageUrl}
+                                                             alt=""/>
+                                                    </Menu.Button>
+                                                </div>
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100 scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95"
+                                                >
+                                                    <Menu.Items
+                                                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                        {userNavigation.map((item) => (
+                                                            <Menu.Item key={item.name}>
+                                                                {({active}) => (
+                                                                    <a
+                                                                        href={item.href}
+                                                                        className={classNames(
+                                                                            active ? 'bg-gray-100' : '',
+                                                                            'block px-4 py-2 text-sm text-gray-700'
+                                                                        )}
+                                                                    >
+                                                                        {item.name}
+                                                                    </a>
+                                                                )}
+                                                            </Menu.Item>
+                                                        ))}
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </Menu>
+                                        </div>
+                                    </div>
+                                    <div className="-mr-2 flex md:hidden">
+                                        {/* Mobile menu button */}
+                                        <Disclosure.Button
+                                            className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                            <span className="sr-only">Open main menu</span>
+                                            {open ? (
+                                                <XIcon className="block h-6 w-6" aria-hidden="true"/>
+                                            ) : (
+                                                <MenuIcon className="block h-6 w-6" aria-hidden="true"/>
+                                            )}
+                                        </Disclosure.Button>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="flex-1 px-2 mx-2">Navbar Title</div>
-                            <div className="flex-none hidden lg:block">
-                                <nav className="menu menu-horizontal">
-                                    <Navigation/>
-                                </nav>
-                            </div>
-                        </div>
+
+                            <Disclosure.Panel className="md:hidden">
+                                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                                    {navigation.map((item) => (
+                                        <Disclosure.Button
+                                            key={item.name}
+                                            as="a"
+                                            href={item.href}
+                                            className={classNames(
+                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                'block px-3 py-2 rounded-md text-base font-medium'
+                                            )}
+                                            aria-current={item.current ? 'page' : undefined}
+                                        >
+                                            {item.name}
+                                        </Disclosure.Button>
+                                    ))}
+                                </div>
+                                <div className="pt-4 pb-3 border-t border-gray-700">
+                                    <div className="flex items-center px-5">
+                                        <div className="flex-shrink-0">
+                                            <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt=""/>
+                                        </div>
+                                        <div className="ml-3">
+                                            <div
+                                                className="text-base font-medium leading-none text-white">{user.name}</div>
+                                            <div
+                                                className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                                        </div>
+                                        <button
+                                            type="button"
+                                            className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                        >
+                                            <span className="sr-only">View notifications</span>
+                                            <BellIcon className="h-6 w-6" aria-hidden="true"/>
+                                        </button>
+                                    </div>
+                                    <div className="mt-3 px-2 space-y-1">
+                                        {userNavigation.map((item) => (
+                                            <Disclosure.Button
+                                                key={item.name}
+                                                as="a"
+                                                href={item.href}
+                                                className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                                            >
+                                                {item.name}
+                                            </Disclosure.Button>
+                                        ))}
+                                    </div>
+                                </div>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+
+                <header className="bg-white shadow">
+                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                    </div>
+                </header>
+                <main className="bg-gray-200 shadow">
+                    <div className="bg-gray-100 max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 border-t border-gray-200">
                         <div className="layout">{props.children}</div>
                     </div>
-                    <div className="drawer-side">
-                        <label htmlFor="my-drawer-3" className="drawer-overlay"/>
-                        <nav className="menu p-4 overflow-y-auto w-80 bg-base-100">
-                            <Navigation/>
-                        </nav>
-                    </div>
-                </div>
-                <style jsx global>{`
-                  html {
-                    box-sizing: border-box;
-                  }
-
-                  *,
-                  *:before,
-                  *:after {
-                    box-sizing: inherit;
-                  }
-
-                  body {
-                    margin: 0;
-                    padding: 0;
-                    font-size: 16px;
-                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-                    Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
-                    "Segoe UI Symbol";
-                    background: rgba(0, 0, 0, 0.05);
-                  }
-
-                  input,
-                  textarea {
-                    font-size: 16px;
-                  }
-
-                  button {
-                    cursor: pointer;
-                  }
-                `}</style>
-                <style jsx>{`
-                  .layout {
-                    padding: 0 2rem;
-                  }
-                `}</style>
+                </main>
             </div>
-        )
-    }
-;
-
-export default Layout;
+        </>
+    )
+}
