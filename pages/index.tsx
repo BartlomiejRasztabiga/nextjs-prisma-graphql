469de92c4a69bd4fbe7f32d3a0cba1afdb853be7
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-05 19:08:38 +0200

    Deploy?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,109 +1,117 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/client";
-import { signIn, signOut, useSession } from "next-auth/react";
+import {useQuery} from "@apollo/client";
+import {signIn, signOut, useSession} from "next-auth/react";
 
 const FeedQuery = gql`
-  query FeedQuery {
-    feed {
-      id
-      title
-      content
-      published
-      author {
-        id
-        name
-      }
+    query FeedQuery {
+        feed {
+            id
+            title
+            content
+            published
+            author {
+                id
+                name
+            }
+        }
     }
-  }
 `;
 
-const Post = ({ post }) => (
-  <Link href="/p/[id]" as={`/p/${post.id}`}>
-    <a>
-      <h2>{post.title}</h2>
-      <small>By {post.author.name}</small>
-      <p>{post.content}</p>
-      <style jsx>{`
-        a {
-          text-decoration: none;
-          color: inherit;
-          padding: 2rem;
-          display: block;
-        }
-      `}</style>
-    </a>
-  </Link>
+const Post = ({post}) => (
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+        <a>
+            <h2>{post.title}</h2>
+            <small>By {post.author.name}</small>
+            <p>{post.content}</p>
+            <style jsx>{`
+              a {
+                text-decoration: none;
+                color: inherit;
+                padding: 2rem;
+                display: block;
+              }
+            `}</style>
+        </a>
+    </Link>
 );
 
 const Blog = () => {
-  const { data: session, status } = useSession();
-  const loading = status === "loading";
+    const {data: session, status} = useSession();
+    const loading = status === "loading";
 
-  const {
-    loading: queryLoading,
-    error,
-    data,
-  } = useQuery(FeedQuery, {
-    fetchPolicy: "cache-and-network",
-  });
+    const {
+        loading: queryLoading,
+        error,
+        data,
+    } = useQuery(FeedQuery, {
+        fetchPolicy: "cache-and-network",
+    });
 
-  if (loading || queryLoading) {
-    return (
-      <div className="flex justify-center mt-8 text-center">
-        <div className="flex-auto">
-          <div className="text-lg mb-2">Loading...</div>
-        </div>
-      </div>
-    );
-  }
-
-  if (!session) {
-    return (
-      <div className="flex justify-center mt-8 text-center">
-        <div className="flex-auto">
-          <div className="text-lg mb-2">You are not logged in!</div>
-          <button className="btn-green" onClick={() => signIn()}>
-            Sign in
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return (
-    <Layout>
-      <div className="page">
-        <h1>My Blog</h1>
-        <main>
-          {data.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
+    if (loading || queryLoading) {
+        return (
+            <div className="flex justify-center mt-8 text-center">
+                <div className="flex-auto">
+                    <div className="text-lg mb-2">Loading...</div>
+                </div>
             </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
+        );
+    }
 
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
+    if (!session) {
+        return (
+            <div className="flex justify-center mt-8 text-center">
+                <div className="flex-auto">
+                    <div className="text-lg mb-2">You are not logged in!</div>
+                    <button className="btn-green" onClick={() => signIn()}>
+                        Sign in
+                    </button>
+                </div>
+            </div>
+        );
+    }
 
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
-  );
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <Layout>
+            <header className="bg-white shadow">
+                <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                </div>
+            </header>
+            <main className="bg-gray-200 shadow">
+                <div className="bg-gray-100 max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 border-t border-gray-200">
+                    <div className="layout">
+                        <main>
+                            {data.feed.map((post) => (
+                                <div key={post.id} className="post">
+                                    <Post post={post}/>
+                                </div>
+                            ))}
+                        </main>
+                    </div>
+                </div>
+            </main>
+            <style jsx>{`
+              .post {
+                background: white;
+                transition: box-shadow 0.1s ease-in;
+              }
+
+              .post:hover {
+                box-shadow: 1px 1px 3px #aaa;
+              }
+
+              .post + .post {
+                margin-top: 2rem;
+              }
+            `}</style>
+        </Layout>
+    );
 };
 
 export default Blog;
