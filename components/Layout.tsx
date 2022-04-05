a8b73c5303e60f597d3fa2393b2760daceb865e9
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-05 19:15:19 +0200

    Deploy?

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,14 +2,12 @@
 import Navbar from "./Navbar";
 
 export default function Layout(props) {
-    return (
-        <>
-            <div className="min-h-full">
-                <Navbar/>
-                <main>
-                    {props.children}
-                </main>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="min-h-full">
+        <Navbar />
+        <main>{props.children}</main>
+      </div>
+    </>
+  );
 }
